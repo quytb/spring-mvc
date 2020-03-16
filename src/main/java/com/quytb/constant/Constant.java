3d2cebf34a4eed1ee4c5bb3c204d61149d3833cd
quytb.hn <quytb.hn@havaz.vn>
2020-03-16 17:24:32 +0700

    them code cho feat3

--- src/main/java/com/quytb/constant/Constant.java
+++ src/main/java/com/quytb/constant/Constant.java
@@ -6,5 +6,6 @@ public class Constant {
     public static final String HOT_FIX = "Hot fix lan 1";
     public static final String FEAT3 = "Nhanh feat3 lan 1";
     public static final String SUN = "Thu 8";
+    public static final String MON = "Thu 2";
 
 }
