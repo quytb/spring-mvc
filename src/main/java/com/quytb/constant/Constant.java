e295be058cb20f2a96c3424d49ce9c62d3c3429b
quytb.hn <quytb.hn@havaz.vn>
2020-03-25 16:23:27 +0700

    commit feat1

--- src/main/java/com/quytb/constant/Constant.java
+++ src/main/java/com/quytb/constant/Constant.java
@@ -10,5 +10,5 @@ public class Constant {
     public static final String TUE = "Thu 3";
     public static final String WED = "Thu 4";
     public static final String THUR = "Thu 5";
-
+    public static final String SUMMER = "Thu 5";
 }
